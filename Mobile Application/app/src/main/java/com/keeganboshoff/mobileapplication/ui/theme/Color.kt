fe459245c2eb604e13f0e94aa04b94e417595e3d
st10443220 <st10443220@myemeris.edu.za>
2026-04-26 23:37:37 +0200

    Created the color palette for the UI within the Color.kt

---
+++ Mobile Application/app/src/main/java/com/keeganboshoff/mobileapplication/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.keeganboshoff.mobileapplication.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+// New colors to be implemented within the application.
+val MidnightBlack = Color(0xFF0A0A0A)
+val ElectricMint = Color(0xFF2ECC71)
+val CyberViolet = Color(0xFF9B59B6)
+val GlassWhite = Color.White.copy(alpha = 0.15f)
