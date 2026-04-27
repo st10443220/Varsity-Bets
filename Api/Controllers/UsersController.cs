794c16a8d2fbb65d209c8ef4428ff3e77f45697f
st10443220 <st10443220@myemeris.edu.za>
2026-04-27 22:04:34 +0200

    Created the UsersController and implemented SyncUser endpoint

---
+++ Api/Controllers/UsersController.cs
@@ -0,0 +1,51 @@
+﻿using Api.Data;
+using Api.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // POST: api/users/sync
+        public async Task<IActionResult> SyncUser([FromBody] UserProfile userProfile)
+        {
+            // Not valid user
+            if (userProfile == null || string.IsNullOrEmpty(userProfile.FirebaseUid))
+            {
+                return BadRequest("Invalid user profile data.");
+            }
+
+            // Check if existing user
+            var existingUser = await _context.UserProfiles.FirstOrDefaultAsync(existingUser =>
+                existingUser.FirebaseUid == userProfile.FirebaseUid
+            );
+
+            // If user null, create new user
+            if (existingUser == null)
+            {
+                _context.UserProfiles.Add(userProfile);
+            }
+            else
+            {
+                // Update the existing user with the new data
+                existingUser.FullName = userProfile.FullName;
+                existingUser.Username = userProfile.Username;
+                existingUser.CreatedAt = userProfile.CreatedAt;
+            }
+
+            await _context.SaveChangesAsync();
+
+            return Ok(userProfile);
+        }
+    }
+}
