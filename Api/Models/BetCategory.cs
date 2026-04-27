accc9187c092e3d704159be54edcf8545bd0b27d
st10443220 <st10443220@myemeris.edu.za>
2026-04-27 19:04:19 +0200

    added the Microsoft.NET Core API

---
+++ Api/Models/BetCategory.cs
@@ -0,0 +1,10 @@
+﻿namespace Api.Models
+{
+    public class BetCategory
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string HexColor { get; set; } = "#2ECC71";
+    }
+}
