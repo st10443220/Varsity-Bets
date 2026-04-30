64cc5e9e8cc4926ad8695d10ea6066ab9825fa0e
st10443220 <st10443220@myemeris.edu.za>
2026-04-30 19:19:59 +0200

    Added the HomeUiState.kt model (because i forgot to click it lol)

---
+++ Mobile Application/app/src/main/java/com/keeganboshoff/mobileapplication/ui/state/HomeUiState.kt
@@ -0,0 +1,9 @@
+package com.keeganboshoff.mobileapplication.ui.state
+
+data class HomeUiState(
+    val fullName: String,
+    val username: String,
+    val sessionCount: Int = 0,
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null
+)
